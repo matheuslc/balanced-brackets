6d2bad721f9763f3d7297b7a20860632428d3e6d
Matheus Carmo <mematheuslc@gmail.com>
2017-03-23 18:20:44 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+export const balance = string => {
+    return true;
+};
---
+++ index.test.js
@@ -0,0 +1,27 @@
+import test from 'ava';
+import { balance } from './index';
+
+test('Should return false', t => {
+	const string = '[{)]';
+
+	// t.false(balance(string));
+	t.false(balance(string));
+});
+
+test('Should return false', t => {
+	const string = '[]{()';
+
+	t.false(balance(string));
+});
+
+test('Should return true', t => {
+	const string = '(){}[]';
+
+	t.true(balance(string));
+});
+
+test('Should return true', t => {
+	const string = '[{()}](){}';
+
+	t.true(balance(string));
+});
